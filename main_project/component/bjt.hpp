31548fba70d955a8c8a65567cb12874bcbb05e7d
ib119 <ib119@ic.ac.uk>
2020-05-24 21:56:06 +0200

    Change BJT to use new method for IV and DIV

--- main_project/component/bjt.hpp
+++ main_project/component/bjt.hpp
@@ -9,7 +9,7 @@
 class BJT: public Component{
 private:
     bool hasVAF;
-    float BF, AF, IS, VAF;
+    float BF, AF, IRS, IFS, VAF;
     float BR = 1.0;
     float VT = 0.025;
 
@@ -20,7 +20,7 @@ private:
         E = 2
     };
 
-    void SetupValues(float BF = 100, float IS = 1e-16, bool hasVAF = false, float VAF = 100);
+    void SetupValues(float BF = 100, float IFS = 1e-16, bool hasVAF = false, float VAF = 100);
 public:
     BJT(string _name, vector<string> args, vector<float> extraInfo);
     BJT(string _name, float r, int n1, int n2);
