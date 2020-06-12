748ef40c86f0fdc3b1ec760ca2d5ed533c32685b
ib119 <ib119@ic.ac.uk>
2020-06-12 15:40:39 +0200

    Add main.cpp changes for efficiency test

--- main_project/main.cpp
+++ main_project/main.cpp
@@ -8,7 +8,46 @@
 
 using namespace std;
 
+void seriesResistors(stringstream& buffer, u_int count){
+    buffer.clear();
+    buffer << "seriesResistors" <<endl;
+    buffer << "V1 n1 0 SIN(0 10 10)" <<endl;
+    for(int i{1}; i<count; i++){
+        buffer << "R" << i << " n" << i << " n" << i+1 << " 1k" <<endl;
+    }
+    buffer << "R" << count << " n" << count << " 0 1k" <<endl;
+    buffer << ".tran 0.0001 0.5 0" <<endl;
+    buffer << ".end" <<endl;
+}
+
+void seriesAcDiode(stringstream& buffer, u_int count){
+    buffer.clear();
+    buffer << "seriesAcDiode" << endl;
+    buffer << ".model D D" << endl;
+    buffer << "V1 n1 0 SIN(0 10 10)" <<endl;
+    for(u_int i{1}; i < count; i++){
+        buffer << "D" << i << " n" << i << " n" << i+1 << " D" <<endl;
+    }
+    buffer << "R" << count << " n" << count << " 0 1k" <<endl;
+    buffer << ".tran 0.001 0.5 0" <<endl;
+    buffer << ".end" <<endl;
+}
+
 int main(int argc, char **argv){
+    // commands for efficiency testing
+    int compCount = 1; // must be 1 at least
+    if(argc == 2){
+        compCount = stoi(argv[1]);
+    }
+
+    stringstream buffer{};
+    
+    // resistors test
+    seriesResistors(buffer, compCount);
+
+    // diode test
+    // seriesAcDiode(buffer, compCount);
+
     //get optional input arguments
     string outputFileName = "out.csv";
     // setup circuit
@@ -17,8 +56,8 @@ int main(int argc, char **argv){
     // stringstream buffer;
     // testCircuit(buffer);
 
-    //  readSpice(c, buffer);
-    readSpice(c, cin); //use this if want to read from txt file
+     readSpice(c, buffer);
+    // readSpice(c, cin); //use this if want to read from txt file
 
     // run simulation
     outputCSV(c, outputFileName);
