9d5b4e03278b62d918d96fa7e73e33fca6378808
ib119 <ib119@ic.ac.uk>
2020-06-12 22:42:04 +0200

    Add runner script for CSVTester

---
+++ Tests/performanceTests/runner.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+./CSVTester
+cp -rf output output1
+./CSVTester
+cp -rf output output2
+./CSVTester
+cp -rf output output3
+./CSVTester
+cp -rf output output4
+./CSVTester
+cp -rf output output5
+./CSVTester
+cp -rf output output6
+./CSVTester
+cp -rf output output7
+./CSVTester
+cp -rf output output8
+./CSVTester
+cp -rf output output9
+./CSVTester
+cp -rf output output10
+./CSVTester
+cp -rf output output11
+./CSVTester
+cp -rf output output12
+./CSVTester
+cp -rf output output13
+./CSVTester
+cp -rf output output14
+./CSVTester
+cp -rf output output15
+./CSVTester
+cp -rf output output16
+./CSVTester
+cp -rf output output17
+./CSVTester
+cp -rf output output18
+./CSVTester
+cp -rf output output19
+./CSVTester
+cp -rf output output20
+./CSVTester
+cp -rf output output31
+./CSVTester
+cp -rf output output32
+./CSVTester
+cp -rf output output33
+./CSVTester
+cp -rf output output34
+./CSVTester
+cp -rf output output35
+./CSVTester
+cp -rf output output36
+./CSVTester
+cp -rf output output37
+./CSVTester
+cp -rf output output38
+./CSVTester
+cp -rf output output39
+./CSVTester
+cp -rf output output40
+./CSVTester
+cp -rf output output41
+./CSVTester
+cp -rf output output42
+./CSVTester
+cp -rf output output43
+./CSVTester
+cp -rf output output44
+./CSVTester
+cp -rf output output45
+./CSVTester
+cp -rf output output46
+./CSVTester
+cp -rf output output47
+./CSVTester
+cp -rf output output48
+./CSVTester
+cp -rf output output49
